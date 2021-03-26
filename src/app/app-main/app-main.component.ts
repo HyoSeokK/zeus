9616dbe2e9feb65ad14c3889389d06bb9f8301e2
eunsukcho <hysces97@gmail.com>
2021-03-26 16:54:09 +0900

    smtp update

--- src/app/app-main/app-main.component.ts
+++ src/app/app-main/app-main.component.ts
@@ -15,6 +15,6 @@ export class AppMainComponent implements OnInit {
     private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://192.168.0.106:31990/#!/state/{%22pinnedMetricType%22:%22CPU%22,%22topologyId%22:%22hosts%22}");
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl("http://192.168.0.106:31401/#!/state/{%22pinnedMetricType%22:%22CPU%22,%22topologyId%22:%22hosts%22}");
   }
 }
