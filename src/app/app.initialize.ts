9df3868e4eb1482b5cba33ed49b303da2fee24ec
eunsukcho <hysces97@gmail.com>
2021-03-26 10:59:30 +0900

    server update info

---
+++ src/app/app.initialize.ts
@@ -0,0 +1,27 @@
+import { AppConfigService } from './services/app-config.service';
+import { KeycloakService } from 'keycloak-angular';
+import { EmailValidator } from '@angular/forms';
+
+export function initializeKeycloak(keycloak: KeycloakService) {
+    return () => 
+      keycloak.init({
+        config: {
+          url: 'https://docker.jointree.co.kr:8443/auth/',
+          realm: 'parthenon',
+          clientId: 'zeus_ces',
+
+        },
+        initOptions : {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+            window.location.origin + '/assets/silent-check-sso.html',
+        },
+        loadUserProfileAtStartUp:true
+    });
+}
+  
+export function initConfig (configService : AppConfigService) {
+    return async () => (await configService.load()).subscribe(response => {
+        localStorage.setItem("env", JSON.stringify(response[0]));
+      });
+}
